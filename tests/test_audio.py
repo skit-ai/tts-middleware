7b9b274373bbbdbb1e5409e9e8dbeaf5602afdbb
Abhinav Tushar <abhinav@lepisma.xyz>
2021-07-04 03:01:35 +0530

    transform pitch and volume

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,6 +1,8 @@
+import operator as op
+
 import numpy as np
 import pytest
-from tts_middleware.audio import transform_rate
+from tts_middleware.audio import transform_rate, transform_volume
 
 
 @pytest.mark.parametrize("duration, rate", [
@@ -15,3 +17,17 @@ def test_transform_rate(duration, rate):
     y = np.random.rand(duration * sample_rate)
 
     assert np.isclose(len(transform_rate(y, sample_rate, rate)) / sample_rate, duration / rate, atol=0.001)
+
+
+@pytest.mark.parametrize("gain, sign_op", [
+    (10, op.gt),
+    (-10, op.lt),
+    (-50, op.lt),
+    (0, np.isclose)
+])
+def test_transform_volume(gain, sign_op):
+    sample_rate = 8000
+    y = np.random.rand(5 * sample_rate)
+    ref = np.mean(y ** 2)
+
+    assert sign_op(np.mean(transform_volume(y, sample_rate, gain) ** 2), ref)
--- tts_middleware/audio.py
+++ tts_middleware/audio.py
@@ -2,9 +2,31 @@ import numpy as np
 import sox
 
 
+def transform_pitch(y: np.ndarray, sample_rate: int, n_semitones: float) -> np.ndarray:
+    """
+    Transform pitch of the given audio chunk and return new chunk. Pitch is
+    changed using the number of semitones, like in sox.
+    """
+
+    tfm = sox.Transformer()
+    tfm.pitch(n_semitones)
+    return tfm.build_array(input_array=y, sample_rate_in=sample_rate)
+
+
+def transform_volume(y: np.ndarray, sample_rate: int, gain_db: float) -> np.ndarray:
+    """
+    Transform loudness of the given audio chunk and return new chunk. `gain_db`
+    must be between -50 and 15.
+    """
+
+    tfm = sox.Transformer()
+    tfm.loudness(gain_db)
+    return tfm.build_array(input_array=y, sample_rate_in=sample_rate)
+
+
 def transform_rate(y: np.ndarray, sample_rate: int, rate: float) -> np.ndarray:
     """
-    Transform speaking rate of the give audio chunk and return new chunk.
+    Transform speaking rate of the given audio chunk and return new chunk.
     """
 
     tfm = sox.Transformer()
