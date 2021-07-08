a0a808030321c6d5f7ae1f90984dc5153f681ec4
Abhinav Tushar <abhinav@lepisma.xyz>
2021-07-09 00:04:51 +0530

    add pitch + volume support

--- tts_middleware/core.py
+++ tts_middleware/core.py
@@ -4,7 +4,8 @@ from typing import Tuple
 import numpy as np
 from pyquery import PyQuery as pq
 
-from tts_middleware.audio import transform_rate
+from tts_middleware.audio import (transform_pitch, transform_rate,
+                                  transform_volume)
 
 # Data array and sample rate
 Audio = Tuple[np.ndarray, int]
@@ -20,15 +21,21 @@ def tts_middleware(tts_function):
         node = pq(text)
         raw_text = node.text()
 
-        rate = None
-        if node("prosody"):
-            if node("prosody").attr.rate:
-                rate = float(node("prosody").attr.rate)
-
         y, sr = tts_function(raw_text)
 
-        if rate is not None:
-            return transform_rate(y, sr, rate), sr
+        if node("prosody"):
+            if node("prosody").attr.pitch:
+                n_semitones = float(node("prosody").attr.pitch)
+                y = transform_pitch(y, sr, n_semitones)
+
+            if node("prosody").attr.rate:
+                rate = float(node("prosody").attr.rate)
+                y = transform_rate(y, sr, rate)
+
+            if node("prosody").attr.volume:
+                gain_db = float(node("prosody").attr.volume)
+                y = transform_volume(y, sr, gain_db)
+
         return y, sr
 
     return _tts
