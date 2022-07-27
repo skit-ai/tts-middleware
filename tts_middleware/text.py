d939e9482b56dd7975e63d86fc2e8f6713edc502
Prabhsimran Singh <pskrunner14@gmail.com>
2022-07-27 15:51:12 +0530

    cleanup | version bump

--- tts_middleware/text.py
+++ tts_middleware/text.py
@@ -1,13 +1,17 @@
 import re
+
 from itertools import repeat
 from concurrent.futures import ThreadPoolExecutor
+
 from google.transliteration import transliterate_text
+
 
 def eng_to_hin(word, lang_code):
     reg = re.compile(r'[a-zA-Z]')
     if reg.match(word):
         return transliterate_text(word, lang_code)
     return word
+
 
 def transliteration(text, lang_code):
     with ThreadPoolExecutor(max_workers=8) as executor:
