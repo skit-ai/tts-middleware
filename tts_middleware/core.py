972ae45fd1eadf5ec4e69ea97af5d463804a0173
Prabhsimran Singh <pskrunner14@gmail.com>
2022-10-06 16:19:38 +0530

    add: parallel audio stitching for longer prompts

--- tts_middleware/core.py
+++ tts_middleware/core.py
@@ -1,8 +1,12 @@
+import re
+from unittest import result
 import numpy as np
 
 from typing import Tuple
 from functools import wraps
 from pyquery import PyQuery as pq
+from pydub import AudioSegment
+from concurrent.futures import ThreadPoolExecutor
 
 from tts_middleware.text import preprocess_text
 from tts_middleware.audio import postprocess_audio
@@ -25,10 +29,15 @@ def tts_middleware(tts_function):
         # Text preprocessing
         text = preprocess_text(text, transliterate=transliterate, language_code=language_code)
 
-        audio, sr = tts_function(
-            text,
-            language_code,
-        )
+        tasks = [(x, language_code) for x in re.split("\.।", text)]
+        results = run_multithreading(tts_function, tasks)
+        
+        audio = AudioSegment.empty()
+        sr = results[0][1]
+        for res in results:
+            audio += res[0]
+
+        audio = audio.get_array_of_samples()
 
         # Audio postprocessing
         audio = postprocess_audio(audio, sr, node("prosody"))
@@ -36,3 +45,9 @@ def tts_middleware(tts_function):
         return audio, sr
 
     return _tts
+
+
+def run_multithreading(func, tasks, num_workers=4):
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
+        results = list(executor.map(func, tasks), total=len(tasks))
+    return results
